ad0a83a19c94ad4dba570ea530e96eae7860fb90
Lucas L <lucas_loe@gmx.de>
2022-09-18 20:39:37 +0200

    onclick events for unread messages

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 console.log('got loaded!')
 
+function countUnreadMessages() {
+    let countUnread = (document.querySelectorAll('.unread')).length
+    console.log(countUnread)
+    let notificationCounter = document.getElementById("notification-counter");
+    notificationCounter.innerHTML = countUnread;
+}
+
 function readAll(){
     let allNotsDots = document.querySelectorAll('#unread-symbol')
     for (const notification of allNotsDots) {
         notification.classList.add('invis')
     }
-    let allNotsContainer = document.querySelectorAll('.unread')
-    for (const notificationContainer of allNotsContainer) {
+    let allUnreadNotsContainer = document.querySelectorAll('.unread')
+    for (const notificationContainer of allUnreadNotsContainer) {
         notificationContainer.classList.remove('unread')
     }
     let notificationCounter = document.getElementById("notification-counter");
     notificationCounter.innerHTML = "0";
+}
+
+function readSingle(e){
+    if (e.target.classList.contains('unread')) {
+        e.target.classList.remove('unread')
+        e.target.querySelector('#unread-symbol').classList.add('invis')
+
+        countUnreadMessages()
+    }
 }
